6d47bf926114bc6246e3eb0812b4e49e35f6ea61
Regan Tan <regantanyk@gmailcom>
2023-07-16 13:20:21 +0800

    automated emails

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from pprint import pprint
+
+
+class NewsFeed:
+    """Representing multiple news titles and links an s a single string
+    """
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "fd2a98ce505b4070900497a4502f4c5b"
+
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
+    
+    def get(self):
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              f"apiKey={self.api_key}"
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+    
+news_feed = NewsFeed(interest='nasa', from_date='2023-07-14', to_date='2023-07-16', language='en')
+print(news_feed.get())
